22a93af5730460d3dbefbf07d4958ed6db80cf95
ShiQing_Chen_Comp <979196931@qq.com>
2018-11-16 10:55:39 +0800

    first commit

---
+++ src/main/java/com/baida/BaidaApplication.java
@@ -0,0 +1,12 @@
+package com.baida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaidaApplication.class, args);
+	}
+}
